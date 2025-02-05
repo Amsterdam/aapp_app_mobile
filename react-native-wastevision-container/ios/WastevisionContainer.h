2a1027b0aecd37830e79c1ad39f593ae73af81bc
Rik Scheffer <r.scheffer@amsterdam.nl>
2025-02-05 10:19:02 +0100

    make swift working inside turbo module

--- react-native-wastevision-container/ios/WastevisionContainer.h
+++ react-native-wastevision-container/ios/WastevisionContainer.h
@@ -1,6 +1,7 @@
-
-#import "generated/RNWastevisionContainerSpec/RNWastevisionContainerSpec.h"
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNWastevisionContainerSpec/RNWastevisionContainerSpec.h"
 
 @interface WastevisionContainer : NativeWastevisionContainerSpecBase <NativeWastevisionContainerSpec>
 
 @end
+#endif
