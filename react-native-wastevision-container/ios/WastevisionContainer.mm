d02266765e531084d36485f3a18f7927816d025c
Rik Scheffer <r.scheffer@amsterdam.nl>
2025-02-05 10:19:02 +0100

    make swift working inside turbo module

--- react-native-wastevision-container/ios/WastevisionContainer.mm
+++ react-native-wastevision-container/ios/WastevisionContainer.mm
@@ -1,4 +1,5 @@
 #import "WastevisionContainer.h"
+#import "react_native_wastevision_container-Swift.h"
 
 @implementation WastevisionContainer
 RCT_EXPORT_MODULE()
@@ -7,6 +8,39 @@ RCT_EXPORT_MODULE()
     NSNumber *result = @(a * b);
 
     return result;
+}
+
+
+- (void)init:(NSString *)servicePrincipalName servicePrincipalSecret:(NSString *)servicePrincipalSecret organisationId:(NSString *)organisationId {
+    SwiftCalculator* calculator = [SwiftCalculator new];
+    NSInteger value = [calculator addWithA:3 b:5];
+    NSNumber* r = @(3+5);
+}
+
+- (void)startScan{
+}
+
+- (void)stopScan{
+}
+
+- (void)dispose{
+}
+
+- (void)autoUnlock:(NSString *)cardNumber{
+}
+
+- (void)checkBluetoothPermission:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject { 
+    
+}
+
+
+- (void)connect:(JS::NativeWastevisionContainer::BluetoothContainerDevice &)bluetoothContainerDevice { 
+    
+}
+
+
+- (void)disconnect:(JS::NativeWastevisionContainer::BluetoothContainerDevice &)bluetoothContainerDevice {
+    
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
---
+++ react-native-wastevision-container/ios/WastevisionContainer.swift
@@ -0,0 +1,7 @@
+@objc
+public class SwiftCalculator: NSObject {
+    @objc
+    public func add(a: Int, b: Int) -> Int {
+        a + b
+    }
+}
