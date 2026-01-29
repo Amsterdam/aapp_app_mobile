fdd18073e15f41e46f3420a8db560a8fcde918e1
Frank Filius <f.filius@amsterdam.nl>
2026-01-29 10:59:32 +0100

    PR comments

--- src/modules/burning-guide/components/BurningGuide.tsx
+++ src/modules/burning-guide/components/BurningGuide.tsx
@@ -31,7 +31,6 @@ export const BurningGuide = () => {
     return (
       <Column gutter="sm">
         <Title
-          level="h2"
           testID="BurningGuideAddressOutsideAmsterdamTitle"
           text="Stookinformatie"
         />
