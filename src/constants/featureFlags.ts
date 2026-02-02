77a587839fc15677a0e88c6d8846389c9e0243f4
Rik Scheffer <r.scheffer@amsterdam.nl>
2026-02-02 11:34:39 +0100

    Enable feature flags

--- src/constants/featureFlags.ts
+++ src/constants/featureFlags.ts
@@ -8,4 +8,8 @@ export enum Features {
  * Features that are in this list will always show or hide base on this value.
  * If a feature is not listed here, it will only show in the development environment.
  */
-export const featureFlags: Partial<Record<Features, boolean>> = {}
+export const featureFlags: Partial<Record<Features, boolean>> = {
+  WasteGuideNotifications: true,
+  WasteGuideCalendarSubscribe: true,
+  BurningGuideNotifications: true,
+}
