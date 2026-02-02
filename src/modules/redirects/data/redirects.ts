e3ab128ace3dea9e232b29db2bfbd567b7aa5b2b
Rik Scheffer <r.scheffer@amsterdam.nl>
2026-02-02 10:52:40 +0100

    Redirects: change elections description

--- src/modules/redirects/data/redirects.ts
+++ src/modules/redirects/data/redirects.ts
@@ -15,7 +15,7 @@ export const redirects: RedirectResponse[] = [
   {
     iconName: 'vote',
     testID: 'RedirectsElectionsButton',
-    text: 'Meer informatie over de verkiezingen voor de gemeenteraad, stadsdeelcommissie en bestuurscommissie Weesp op woensdag 18 maart 2026.',
+    text: '18 maart 2026',
     title: 'Verkiezingen',
     urlKey: RedirectKey.elections,
   },
