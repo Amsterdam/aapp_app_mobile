28f483a84bc9842a2a658eae3dfe8d419ee8bf9e
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-02-04 14:58:34 +0100

    Add redirect keys for ExternalLinkButton usage

--- src/modules/report-problem/components/MyReportsSection.tsx
+++ src/modules/report-problem/components/MyReportsSection.tsx
@@ -4,44 +4,37 @@ import {Row} from '@/components/ui/layout/Row'
 import {Track} from '@/components/ui/layout/Track'
 import {Paragraph} from '@/components/ui/text/Paragraph'
 import {Title} from '@/components/ui/text/Title'
-import {useOpenWebUrl} from '@/hooks/linking/useOpenWebUrl'
-import {useUrlForEnv} from '@/hooks/useUrlForEnv'
-import {myReportsExternalLinks} from '@/modules/report-problem/external-links'
+import {RedirectKey} from '@/modules/redirects/types'
 
-export const MyReportsSection = () => {
-  const openWebUrl = useOpenWebUrl()
-  const reportProblemMapUrl = useUrlForEnv(myReportsExternalLinks)
-
-  return (
-    <Column gutter="md">
-      <Title
-        level="h2"
-        testID="MyReportsSectionTitle"
-        text="Melding volgen"
-      />
-      <Track gutter="lg">
-        <Column
-          flex={2}
-          gutter="md">
-          <Paragraph>U kunt uw melding volgen via Mijn meldingen.</Paragraph>
-        </Column>
-        <Column
-          flex={1}
-          gutter="md">
-          <Row
-            gutter="md"
-            wrap>
-            <Column grow={1}>
-              <ExternalLinkButton
-                label="Mijn meldingen"
-                onPress={() => openWebUrl(reportProblemMapUrl)}
-                testID="MyReportsSectionExternalLinkButton"
-                variant="secondary"
-              />
-            </Column>
-          </Row>
-        </Column>
-      </Track>
-    </Column>
-  )
-}
+export const MyReportsSection = () => (
+  <Column gutter="md">
+    <Title
+      level="h2"
+      testID="MyReportsSectionTitle"
+      text="Melding volgen"
+    />
+    <Track gutter="lg">
+      <Column
+        flex={2}
+        gutter="md">
+        <Paragraph>U kunt uw melding volgen via Mijn meldingen.</Paragraph>
+      </Column>
+      <Column
+        flex={1}
+        gutter="md">
+        <Row
+          gutter="md"
+          wrap>
+          <Column grow={1}>
+            <ExternalLinkButton
+              label="Mijn meldingen"
+              redirectKey={RedirectKey.my_reported_problems}
+              testID="MyReportsSectionExternalLinkButton"
+              variant="secondary"
+            />
+          </Column>
+        </Row>
+      </Column>
+    </Track>
+  </Column>
+)
--- src/modules/report-problem/components/ReportMapSection.tsx
+++ src/modules/report-problem/components/ReportMapSection.tsx
@@ -4,47 +4,40 @@ import {Row} from '@/components/ui/layout/Row'
 import {Track} from '@/components/ui/layout/Track'
 import {Paragraph} from '@/components/ui/text/Paragraph'
 import {Title} from '@/components/ui/text/Title'
-import {useOpenWebUrl} from '@/hooks/linking/useOpenWebUrl'
-import {useUrlForEnv} from '@/hooks/useUrlForEnv'
-import {reportProblemMapExternalLinks} from '@/modules/report-problem/external-links'
+import {RedirectKey} from '@/modules/redirects/types'
 
-export const ReportMapSection = () => {
-  const openWebUrl = useOpenWebUrl()
-  const reportProblemMapUrl = useUrlForEnv(reportProblemMapExternalLinks)
-
-  return (
-    <Column gutter="md">
-      <Title
-        level="h2"
-        testID="ReportMapSectionTitle"
-        text="Meldingenkaart"
-      />
-      <Track gutter="lg">
-        <Column
-          flex={2}
-          gutter="md">
-          <Paragraph>
-            Bekijk eerst de meldingenkaart om te zien welke meldingen al bekend
-            zijn bij de gemeente.
-          </Paragraph>
-        </Column>
-        <Column
-          flex={1}
-          gutter="md">
-          <Row
-            gutter="md"
-            wrap>
-            <Column grow={1}>
-              <ExternalLinkButton
-                label="Meldingenkaart"
-                onPress={() => openWebUrl(reportProblemMapUrl)}
-                testID="ReportMapSectionExternalLinkButton"
-                variant="secondary"
-              />
-            </Column>
-          </Row>
-        </Column>
-      </Track>
-    </Column>
-  )
-}
+export const ReportMapSection = () => (
+  <Column gutter="md">
+    <Title
+      level="h2"
+      testID="ReportMapSectionTitle"
+      text="Meldingenkaart"
+    />
+    <Track gutter="lg">
+      <Column
+        flex={2}
+        gutter="md">
+        <Paragraph>
+          Bekijk eerst de meldingenkaart om te zien welke meldingen al bekend
+          zijn bij de gemeente.
+        </Paragraph>
+      </Column>
+      <Column
+        flex={1}
+        gutter="md">
+        <Row
+          gutter="md"
+          wrap>
+          <Column grow={1}>
+            <ExternalLinkButton
+              label="Meldingenkaart"
+              redirectKey={RedirectKey.reported_problems_map}
+              testID="ReportMapSectionExternalLinkButton"
+              variant="secondary"
+            />
+          </Column>
+        </Row>
+      </Column>
+    </Track>
+  </Column>
+)
