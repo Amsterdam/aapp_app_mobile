1ece40010ace5da39bf8001b10ef171575e17b03
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-02-11 16:05:07 +0100

    Create AccessCodeWrapper to render access code flow conditionally

--- src/modules/parking/Stack.tsx
+++ src/modules/parking/Stack.tsx
@@ -1,6 +1,13 @@
-import {TransitionPresets} from '@react-navigation/stack'
+import {
+  TransitionPresets,
+  type StackNavigationOptions,
+} from '@react-navigation/stack'
+import {useMemo, type ReactNode} from 'react'
 import {createStackNavigator} from '@/app/navigation/createStackNavigator'
-import {RootStackParams} from '@/app/navigation/types'
+import {
+  RootStackParams,
+  type HeaderContentOptions,
+} from '@/app/navigation/types'
 import {useScreenOptions} from '@/app/navigation/useScreenOptions'
 import {usePendingScreen} from '@/hooks/navigation/usePendingScreen'
 import {useAccessCodeBiometrics} from '@/modules/access-code/hooks/useAccessCodeBiometrics'
@@ -14,7 +21,10 @@ import {ConfirmAccessCodeScreen} from '@/modules/access-code/screens/ConfirmAcce
 import {SetAccessCodeScreen} from '@/modules/access-code/screens/SetAccessCode.screen'
 import {useIsRecentlyLoggedOut} from '@/modules/parking/hooks/useIsRecentlyLoggedOut'
 import {useLoginSteps} from '@/modules/parking/hooks/useLoginSteps'
-import {ParkingRouteName} from '@/modules/parking/routes'
+import {
+  ParkingRouteName,
+  type ParkingStackParams,
+} from '@/modules/parking/routes'
 import {
   parkingScreenConfig,
   type ParkingScreenConfigRoutes,
@@ -29,13 +39,7 @@ import {sortEntriesByKeyFirst} from '@/utils/sortEntriesByKeyFirst'
 
 const Stack = createStackNavigator<RootStackParams>()
 
-// eslint-disable-next-line sonarjs/cognitive-complexity
 export const ParkingStack = () => {
-  const {accessCode, isLoading} = useGetSecureAccessCode()
-  const {attemptsLeft, isCodeValid, isForgotCode} = useEnterAccessCode()
-  const {isEnrolled, useBiometrics} = useAccessCodeBiometrics()
-  const {isLoginStepsActive} = useLoginSteps()
-  const {isRecentlyLoggedOut} = useIsRecentlyLoggedOut()
   const screenOptions = useScreenOptions()
   const isLoggingIn = useParkingAccountIsLoggingIn()
   const hasAccounts = useIsLoggedIn()
@@ -44,95 +48,169 @@ export const ParkingStack = () => {
     Object.entries(parkingScreenConfig),
     pendingScreen,
   )
+  const {isLoginStepsActive} = useLoginSteps()
+
+  const {isRecentlyLoggedOut} = useIsRecentlyLoggedOut()
+
+  return (
+    <AccessCodeWrapper
+      hide={!hasAccounts}
+      isLoginStepsActive={isLoginStepsActive}
+      loginSteps={{
+        [ParkingRouteName.loginSteps]: {
+          component: LoginStepsScreen,
+          name: ParkingRouteName.loginSteps,
+          options: {
+            headerTitle: 'Inloggen',
+          },
+        },
+      }}>
+      <Stack.Navigator screenOptions={screenOptions}>
+        {hasAccounts ? (
+          <Stack.Group>
+            {/*Logged in */}
+            {!!isLoggingIn && (
+              <Stack.Screen
+                component={ParkingLoginScreen}
+                name={ParkingRouteName.login}
+                options={{headerTitle: 'Inloggen'}}
+              />
+            )}
+            {sortedScreenConfig.map(([key, parkingRoute]) => (
+              <Stack.Screen
+                key={key}
+                {...parkingRoute}
+              />
+            ))}
+          </Stack.Group>
+        ) : (
+          <Stack.Group>
+            {/*Logged out */}
+            {!isRecentlyLoggedOut && (
+              <Stack.Screen
+                component={ParkingIntroScreen}
+                name={ParkingRouteName.intro}
+                options={{headerTitle: 'Aanmelden parkeren'}}
+              />
+            )}
+            <Stack.Screen
+              component={ParkingLoginScreen}
+              name={ParkingRouteName.login}
+              options={{headerTitle: 'Inloggen'}}
+            />
+          </Stack.Group>
+        )}
+      </Stack.Navigator>
+    </AccessCodeWrapper>
+  )
+}
+
+const AccessCodeStack = createStackNavigator<RootStackParams>()
+
+const AccessCodeWrapper = ({
+  children,
+  hide,
+  isLoginStepsActive,
+  loginSteps,
+}: {
+  children: ReactNode
+  hide?: boolean
+  isLoginStepsActive?: boolean
+  loginSteps?: Record<
+    string,
+    {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      component: React.ComponentType<any>
+      name: keyof ParkingStackParams
+      options?: StackNavigationOptions & HeaderContentOptions
+      screenType?: 'default' | 'settings'
+      title?: string
+    }
+  >
+}) => {
+  const screenOptions = useScreenOptions()
+
+  const {accessCode, isLoading} = useGetSecureAccessCode()
+  const {attemptsLeft, isCodeValid, isForgotCode} = useEnterAccessCode()
+  const {isEnrolled, useBiometrics} = useAccessCodeBiometrics()
+
+  const accessCodeStackSwitch = useMemo(() => {
+    if (isForgotCode) {
+      return (
+        <AccessCodeStack.Screen
+          component={ParkingForgotAccessCodeScreen}
+          name={ParkingRouteName.forgotAccessCode}
+          options={{headerTitle: 'Toegangscode vergeten'}}
+        />
+      )
+    }
+
+    if (!accessCode || isLoginStepsActive) {
+      return (
+        <AccessCodeStack.Group>
+          {!!loginSteps &&
+            Object.entries(loginSteps).map(([key, parkingRoute]) => (
+              <AccessCodeStack.Screen
+                key={key}
+                {...parkingRoute}
+              />
+            ))}
+          <AccessCodeStack.Screen
+            component={SetAccessCodeScreen}
+            name={AccessCodeRouteName.setAccessCode}
+            options={{headerTitle: 'Toegangscode kiezen'}}
+          />
+          <AccessCodeStack.Screen
+            component={ConfirmAccessCodeScreen}
+            name={AccessCodeRouteName.confirmAccessCode}
+            options={{headerTitle: 'Toegangscode herhalen'}}
+          />
+        </AccessCodeStack.Group>
+      )
+    }
+
+    if (attemptsLeft > 0) {
+      return (
+        <AccessCodeStack.Screen
+          component={AccessCodeScreen}
+          name={AccessCodeRouteName.accessCode}
+          options={{
+            headerTitle: 'Toegangscode invoeren',
+            ...TransitionPresets.ModalFadeTransition,
+          }}
+        />
+      )
+    }
+
+    return (
+      <AccessCodeStack.Screen
+        component={AccessCodeInvalidScreen}
+        name={AccessCodeRouteName.accessCodeInvalid}
+      />
+    )
+  }, [isForgotCode, accessCode, attemptsLeft, loginSteps, isLoginStepsActive])
 
   if (isLoading) {
     return null
   }
 
+  if (hide || isCodeValid) {
+    return children
+  }
+
   return (
-    <Stack.Navigator screenOptions={screenOptions}>
-      {isForgotCode ? (
-        <Stack.Screen
-          component={ParkingForgotAccessCodeScreen}
-          name={ParkingRouteName.forgotAccessCode}
-          options={{headerTitle: 'Toegangscode vergeten'}}
-        />
-      ) : useBiometrics === undefined && isEnrolled && isCodeValid ? (
-        <Stack.Screen
+    <AccessCodeStack.Navigator screenOptions={screenOptions}>
+      {accessCodeStackSwitch}
+
+      {useBiometrics === undefined && !!isEnrolled && !!isCodeValid && (
+        <AccessCodeStack.Screen
           component={BiometricsPermissionScreen}
           name={AccessCodeRouteName.biometricsPermission}
           options={{
             headerTitle: 'Sneller toegang',
           }}
         />
-      ) : hasAccounts ? (
-        accessCode && !isLoginStepsActive ? (
-          isCodeValid ? (
-            <>
-              {!!isLoggingIn && (
-                <Stack.Screen
-                  component={ParkingLoginScreen}
-                  name={ParkingRouteName.login}
-                  options={{headerTitle: 'Inloggen'}}
-                />
-              )}
-              {sortedScreenConfig.map(([key, parkingRoute]) => (
-                <Stack.Screen
-                  key={key}
-                  {...parkingRoute}
-                />
-              ))}
-            </>
-          ) : attemptsLeft > 0 ? (
-            <Stack.Screen
-              component={AccessCodeScreen}
-              name={AccessCodeRouteName.accessCode}
-              options={{
-                headerTitle: 'Toegangscode invoeren',
-                ...TransitionPresets.ModalFadeTransition,
-              }}
-            />
-          ) : (
-            <Stack.Screen
-              component={AccessCodeInvalidScreen}
-              name={AccessCodeRouteName.accessCodeInvalid}
-            />
-          )
-        ) : (
-          <>
-            <Stack.Screen
-              component={LoginStepsScreen}
-              name={ParkingRouteName.loginSteps}
-              options={{headerTitle: 'Inloggen'}}
-            />
-            <Stack.Screen
-              component={SetAccessCodeScreen}
-              name={AccessCodeRouteName.setAccessCode}
-              options={{headerTitle: 'Toegangscode kiezen'}}
-            />
-            <Stack.Screen
-              component={ConfirmAccessCodeScreen}
-              name={AccessCodeRouteName.confirmAccessCode}
-              options={{headerTitle: 'Toegangscode herhalen'}}
-            />
-          </>
-        )
-      ) : (
-        <>
-          {!isRecentlyLoggedOut && (
-            <Stack.Screen
-              component={ParkingIntroScreen}
-              name={ParkingRouteName.intro}
-              options={{headerTitle: 'Aanmelden parkeren'}}
-            />
-          )}
-          <Stack.Screen
-            component={ParkingLoginScreen}
-            name={ParkingRouteName.login}
-            options={{headerTitle: 'Inloggen'}}
-          />
-        </>
       )}
-    </Stack.Navigator>
+    </AccessCodeStack.Navigator>
   )
 }
