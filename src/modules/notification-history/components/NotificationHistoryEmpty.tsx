0ca95e3fe31759b58c24b921bdd6cc1eac07a19d
Frank Filius <f.filius@amsterdam.nl>
2026-02-12 14:52:56 +0100

    Remove image when no notifications

--- src/modules/notification-history/components/NotificationHistoryEmpty.tsx
+++ src/modules/notification-history/components/NotificationHistoryEmpty.tsx
@@ -1,35 +1,23 @@
-import AdamEvaCheerBlue from '@/assets/images/adam-eva-cheer-blue.svg'
-import {Box} from '@/components/ui/containers/Box'
 import {SingleSelectable} from '@/components/ui/containers/SingleSelectable'
+import {Center} from '@/components/ui/layout/Center'
 import {Column} from '@/components/ui/layout/Column'
-import {Gutter} from '@/components/ui/layout/Gutter'
-import {FigureWithFacadesBackground} from '@/components/ui/media/FigureWithFacadesBackground'
-import {Paragraph} from '@/components/ui/text/Paragraph'
+import {Phrase} from '@/components/ui/text/Phrase'
 import {Title} from '@/components/ui/text/Title'
 
 export const NotificationHistoryEmpty = () => (
-  <Box
-    grow
-    inset="no"
-    testID="NotificationHistoryEmptyContainer">
-    <Box>
-      <Gutter height="xxl" />
-      <SingleSelectable>
-        <Column halign="center">
-          <Title
-            level="h2"
-            text="U heeft geen meldingen"
-          />
-          <Paragraph variant="intro">In de afgelopen 30 dagen</Paragraph>
-        </Column>
-      </SingleSelectable>
-    </Box>
-    <Column
-      align="center"
-      grow={1}>
-      <FigureWithFacadesBackground testID="NotificationHistoryEmptyImage">
-        <AdamEvaCheerBlue />
-      </FigureWithFacadesBackground>
-    </Column>
-  </Box>
+  <Center grow>
+    <SingleSelectable>
+      <Column
+        gutter="sm"
+        halign="center">
+        <Title
+          level="h4"
+          text="Geen meldingen"
+        />
+        <Phrase testID="NotificationHistoryEmptyPhrase">
+          In de afgelopen 30 dagen
+        </Phrase>
+      </Column>
+    </SingleSelectable>
+  </Center>
 )
