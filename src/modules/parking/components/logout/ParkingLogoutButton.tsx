01b54bd00dd770065bde66e0af4b92dbc739b9ce
Frank Filius <f.filius@amsterdam.nl>
2026-02-20 16:53:41 +0100

    Navigate to my accounts screen when last parking-account is logged out

--- src/modules/parking/components/logout/ParkingLogoutButton.tsx
+++ src/modules/parking/components/logout/ParkingLogoutButton.tsx
@@ -4,16 +4,20 @@ import {useDispatch} from '@/hooks/redux/useDispatch'
 import {useStore} from '@/hooks/redux/useStore'
 import {alerts} from '@/modules/parking/alerts'
 import {logout} from '@/modules/parking/utils/logout'
+import {ModuleSlug} from '@/modules/slugs'
+import {UserRouteName} from '@/modules/user/routes'
 import {useAlert} from '@/store/slices/alert'
 
 type Props = {
-  accountReportCode: string
+  accountReportCode?: string
   hasMoreAccounts: boolean
+  routeReportCode?: string
 }
 
 export const ParkingLogoutButton = ({
   accountReportCode,
   hasMoreAccounts,
+  routeReportCode,
 }: Props) => {
   const navigation = useNavigation()
   const dispatch = useDispatch()
@@ -29,6 +33,9 @@ export const ParkingLogoutButton = ({
         if (hasMoreAccounts) {
           setAlert(alerts.logoutWithAnotherAccountSuccess)
           navigation.goBack()
+        } else {
+          routeReportCode &&
+            navigation.popTo(ModuleSlug.user, {screen: UserRouteName.accounts})
         }
       }}
       testID="ParkingLogoutScreenLogoutButton"
