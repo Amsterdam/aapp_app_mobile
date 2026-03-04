9f06bd91748218493e208b1949143d55df0244cf
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-03-04 12:43:11 +0100

    Set up service module in app

--- src/modules/generated/modals.generated.ts
+++ src/modules/generated/modals.generated.ts
@@ -4,9 +4,10 @@ import {modals as modals2} from '@/modules/construction-work-editor/screenConfig
 import {modals as modals3} from '@/modules/contact/screenConfig'
 import {modals as modals4} from '@/modules/home/screenConfig'
 import {modals as modals5} from '@/modules/report-problem/screenConfig'
-import {modals as modals6} from '@/modules/user/screenConfig'
-import {modals as modals7} from '@/modules/waste-container/screenConfig'
-import {modals as modals8} from '@/modules/waste-guide/screenConfig'
+import {modals as modals6} from '@/modules/service/screenConfig'
+import {modals as modals7} from '@/modules/user/screenConfig'
+import {modals as modals8} from '@/modules/waste-container/screenConfig'
+import {modals as modals9} from '@/modules/waste-guide/screenConfig'
 
 export const modals = {
   ...modals0,
@@ -18,4 +19,5 @@ export const modals = {
   ...modals6,
   ...modals7,
   ...modals8,
+  ...modals9,
 }
---
+++ src/modules/service/Stack.tsx
@@ -0,0 +1,24 @@
+import {createStackNavigator} from '@/app/navigation/createStackNavigator'
+import {RootStackParams} from '@/app/navigation/types'
+import {useScreenOptions} from '@/app/navigation/useScreenOptions'
+import {ServiceRouteName} from '@/modules/service/routes'
+import {screenConfig} from '@/modules/service/screenConfig'
+
+const Stack = createStackNavigator<RootStackParams>()
+
+export const ServiceStack = () => {
+  const screenOptions = useScreenOptions()
+
+  return (
+    <Stack.Navigator
+      initialRouteName={ServiceRouteName.overview}
+      screenOptions={screenOptions}>
+      {Object.entries(screenConfig).map(([key, route]) => (
+        <Stack.Screen
+          key={key}
+          {...route}
+        />
+      ))}
+    </Stack.Navigator>
+  )
+}
