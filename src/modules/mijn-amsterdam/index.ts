e6eb1da3cb2d376c359d62c6865f3c9c73b753fc
Frank Filius <f.filius@amsterdam.nl>
2026-03-16 09:52:30 +0100

    Exclude profile name from persisting to improve privacy

--- src/modules/mijn-amsterdam/index.ts
+++ src/modules/mijn-amsterdam/index.ts
@@ -1,15 +1,24 @@
-import {mijnAmsterdamSlice} from '@/modules/mijn-amsterdam/slice'
+import {
+  mijnAmsterdamSlice,
+  type MijnAmsterdamState,
+} from '@/modules/mijn-amsterdam/slice'
 import {logout} from '@/modules/mijn-amsterdam/utils/logout'
 import {ModuleSlug} from '@/modules/slugs'
 import {UserRouteName} from '@/modules/user/routes'
 import {createClientModule} from '@/modules/utils/createModule'
 import {ReduxKey} from '@/store/types/reduxKey'
 
+const persistWhitelist: (keyof MijnAmsterdamState)[] = [
+  'isLoggedIn',
+  'shouldShowBanner',
+]
+
 export const mijnAmsterdamModule = createClientModule({
   reduxConfigs: [
     {
       key: ReduxKey.mijnAmsterdam,
       persistVersion: 0,
+      persistWhitelist,
       slice: mijnAmsterdamSlice,
     },
   ],
