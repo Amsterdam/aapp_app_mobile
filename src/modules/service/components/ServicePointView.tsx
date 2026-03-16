032cf9eea38cf76987fa238dd872e1e575721970
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-03-16 10:49:40 +0100

    Implement ServicePointListItem component and integrate with ServicePointList and ServicePointMap

--- src/modules/service/components/ServicePointView.tsx
+++ src/modules/service/components/ServicePointView.tsx
@@ -1,14 +1,34 @@
+import {useCallback} from 'react'
 import type {ServiceItem} from '@/modules/service/types'
 import {MapViewVariant} from '@/components/features/map/MapViewSwitchContext'
 import {useMapViewSwitch} from '@/components/features/map/hooks/useMapViewSwitch'
+import {useDispatch} from '@/hooks/redux/useDispatch'
 import {ServicePointList} from '@/modules/service/components/ServicePointList'
 import {ServicePointMap} from '@/modules/service/components/ServicePointMap'
+import {setSelectedServicePointId} from '@/modules/service/slice'
+import {useBottomSheet} from '@/store/slices/bottomSheet'
 
 export const ServicePointView = (props: {id: ServiceItem['id']}) => {
   const {viewType} = useMapViewSwitch()
 
+  const dispatch = useDispatch()
+  const {open} = useBottomSheet()
+
+  const onServicePointPress = useCallback(
+    (id: string) => {
+      dispatch(setSelectedServicePointId(id))
+      open()
+    },
+    [dispatch, open],
+  )
+
   const ServiceViewComponent =
     viewType === MapViewVariant.map ? ServicePointMap : ServicePointList
 
-  return <ServiceViewComponent {...props} />
+  return (
+    <ServiceViewComponent
+      {...props}
+      onServicePointPress={onServicePointPress}
+    />
+  )
 }
