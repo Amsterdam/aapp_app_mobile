28c82c559d2e9b18d0f4659a2d499bcf417d9a1d
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-03-18 11:58:57 +0100

    Add useLocation hook and set initialRegion

--- src/modules/service/components/ServicePointMap.tsx
+++ src/modules/service/components/ServicePointMap.tsx
@@ -8,6 +8,8 @@ import {MapFilters} from '@/components/features/map/filters/MapFilters'
 import {useMapFilters} from '@/components/features/map/hooks/useMapFilters'
 import {ControlVariant} from '@/components/features/map/types'
 import {PleaseWait} from '@/components/ui/feedback/PleaseWait'
+import {useGetLocation} from '@/modules/address/hooks/useGetLocation'
+import {useLocation} from '@/modules/address/slice'
 import {useGetMapData} from '@/modules/service/hooks/useGetMapData'
 import {useServiceQuery} from '@/modules/service/service'
 import {ModuleSlug} from '@/modules/slugs'
@@ -28,6 +30,10 @@ export const ServicePointMap = ({
   } = useServiceQuery(serviceId || skipToken)
   const geojson = service?.data
 
+  useGetLocation()
+  // const {address} = useSelectedAddress(ModuleSlug.service)
+  const {location} = useLocation()
+
   const {activeFilters, filters, onPressFilter} = useMapFilters()
   const data = useGetMapData(activeFilters, geojson, onServicePointPress)
 
@@ -45,6 +51,16 @@ export const ServicePointMap = ({
           onPressFilter={onPressFilter}
           testID="ServiceMapFilters"
         />
+      }
+      initialRegion={
+        location?.coordinates
+          ? {
+              latitude: location?.coordinates.lat,
+              longitude: location?.coordinates.lon,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01,
+            }
+          : undefined
       }
       isError={isError || !data?.length}
       moduleSlug={ModuleSlug.service}
