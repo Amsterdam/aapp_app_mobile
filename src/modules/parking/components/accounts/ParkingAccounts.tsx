066f468036400d1f27dc1acbe28cf6d78e0eb1cb
Frank Filius <f.filius@amsterdam.nl>
2026-03-23 11:17:43 +0100

    Improve readability

--- src/modules/parking/components/accounts/ParkingAccounts.tsx
+++ src/modules/parking/components/accounts/ParkingAccounts.tsx
@@ -1,3 +1,4 @@
+import {useMemo} from 'react'
 import {Column} from '@/components/ui/layout/Column'
 import {Paragraph} from '@/components/ui/text/Paragraph'
 import {ParkingAccountsByScope} from '@/modules/parking/components/accounts/ParkingAccountsByScope'
@@ -6,11 +7,16 @@ import {ParkingPermitScope} from '@/modules/parking/types'
 
 export const ParkingAccounts = () => {
   const parkingAccounts = useParkingAccounts()
-  const permitHolderAccounts = Object.values(parkingAccounts).filter(
-    account => account.scope === ParkingPermitScope.permitHolder,
-  )
-  const visitorAccounts = Object.values(parkingAccounts).filter(
-    account => account.scope === ParkingPermitScope.visitor,
+  const [permitHolderAccounts, visitorAccounts] = useMemo(
+    () => [
+      Object.values(parkingAccounts).filter(
+        account => account.scope === ParkingPermitScope.permitHolder,
+      ),
+      Object.values(parkingAccounts).filter(
+        account => account.scope === ParkingPermitScope.visitor,
+      ),
+    ],
+    [parkingAccounts],
   )
 
   if (!permitHolderAccounts.length && !visitorAccounts.length) {
