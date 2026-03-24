2c8a514a7e61412cfd4add3d7d8cb386b7246c51
Rik Scheffer <r.scheffer@amsterdam.nl>
2026-03-24 10:45:28 +0100

    fix TextInput typescript typing

--- src/components/ui/forms/SearchField.tsx
+++ src/components/ui/forms/SearchField.tsx
@@ -1,11 +1,10 @@
 import {forwardRef, useState} from 'react'
 import {
+  type BlurEvent,
   type GestureResponderEvent,
-  type NativeSyntheticEvent,
   StyleSheet,
   TextInput,
-  type TextInputFocusEventData,
-  type TextInputKeyPressEventData,
+  type TextInputKeyPressEvent,
   type TextInputProps,
   View,
 } from 'react-native'
@@ -53,7 +52,7 @@ export const SearchField = forwardRef<TextInput, SearchFieldProps>(
       },
     })
 
-    const onBlur = (event: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    const onBlur = (event: BlurEvent) => {
       setHasFocus(false)
       onEvent(event)
       textInputProps.onBlur?.(event)
@@ -70,14 +69,12 @@ export const SearchField = forwardRef<TextInput, SearchFieldProps>(
       onEvent(event)
     }
 
-    const onFocus = (e: NativeSyntheticEvent<TextInputFocusEventData>) => {
+    const onFocus = (e: FocusEvent) => {
       setHasFocus(true)
       textInputProps.onFocus?.(e)
     }
 
-    const handleBackspaceKeyPress = (
-      event: NativeSyntheticEvent<TextInputKeyPressEventData>,
-    ) => {
+    const handleBackspaceKeyPress = (event: TextInputKeyPressEvent) => {
       if (event.nativeEvent.key === 'Backspace') {
         onEvent(event)
       }
