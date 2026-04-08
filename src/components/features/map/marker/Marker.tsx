4836563368bda0922ec364272874f8e6c6e27c4c
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-04-08 14:29:45 +0200

    Remove deprecated marker settings; Improve marker anchor

--- src/components/features/map/marker/Marker.tsx
+++ src/components/features/map/marker/Marker.tsx
@@ -1,4 +1,5 @@
 import {memo} from 'react'
+import {Platform} from 'react-native'
 import {
   // eslint-disable-next-line no-restricted-imports
   Marker as MarkerRN,
@@ -7,18 +8,20 @@ import {
 } from 'react-native-maps'
 import {checkMarkerPropsHaveChanged} from '@/components/features/map/utils/checkMarkerPropsHaveChanged'
 
-const GOOGLE_MAPS_MARKER_OFFSET: Point = {x: 0.25, y: 1}
-const APPLE_MAPS_MARKER_OFFSET: Point = {x: 0, y: -20}
+const MARKER_ANCHOR: Point = {x: 0.5, y: 1}
 const DEFAULT_HIT_SLOP = 20
 
-export type MarkerProps = Omit<MapMarkerProps, 'icon' | 'image'>
+export type MarkerProps = Omit<
+  MapMarkerProps,
+  'icon' | 'image' | 'tracksViewChanges'
+>
 
 export const Marker = memo(
   ({hitSlop = DEFAULT_HIT_SLOP, ...markerProps}: MarkerProps) => (
     <MarkerRN
-      anchor={GOOGLE_MAPS_MARKER_OFFSET}
-      centerOffset={APPLE_MAPS_MARKER_OFFSET}
+      anchor={MARKER_ANCHOR}
       hitSlop={hitSlop}
+      tracksViewChanges={Platform.OS === 'android'}
       {...markerProps}
     />
   ),
