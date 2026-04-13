42393f0573bf85b46bbc13c0a3099c3ee5da2870
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-04-13 11:01:44 +0200

    Make iconName required

--- src/modules/home/types.ts
+++ src/modules/home/types.ts
@@ -2,7 +2,7 @@ import {SvgIconName} from '@/components/ui/media/svgIcons'
 import {Permissions} from '@/types/permissions'
 
 export type PermissionInstructionScreenParams = {
-  iconName?: SvgIconName
+  iconName: SvgIconName
   paragraph: string
   permission: Permissions
   screenTitle: string
