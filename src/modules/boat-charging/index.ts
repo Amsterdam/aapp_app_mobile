954a5b0477906760bc5fad5dfef6054751e98ce7
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-05-06 09:56:40 +0200

    Move module icon to svgIcons list

--- src/modules/boat-charging/index.ts
+++ src/modules/boat-charging/index.ts
@@ -1,9 +1,7 @@
-import {boatChargingSvgIcons} from '@/modules/boat-charging/icons'
 import {ModuleSlug} from '@/modules/slugs'
 import {createClientModule} from '@/modules/utils/createModule'
 
 export const boatChargingModule = createClientModule({
-  icons: boatChargingSvgIcons,
   name: 'BoatChargingModule',
   slug: ModuleSlug['boat-charging'],
 })
