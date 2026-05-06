2392991dd529b92b7eb82828ce4745dd62ed4296
Frank Filius <f.filius@amsterdam.nl>
2026-05-06 09:05:44 +0200

    Add module icon

--- src/modules/boat-charging/index.ts
+++ src/modules/boat-charging/index.ts
@@ -1,7 +1,9 @@
+import {boatChargingSvgIcons} from '@/modules/boat-charging/icons'
 import {ModuleSlug} from '@/modules/slugs'
 import {createClientModule} from '@/modules/utils/createModule'
 
 export const boatChargingModule = createClientModule({
+  icons: boatChargingSvgIcons,
   name: 'BoatChargingModule',
   slug: ModuleSlug['boat-charging'],
 })
