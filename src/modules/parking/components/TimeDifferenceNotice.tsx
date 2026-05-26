f98dee6867ac7c36e20a3e557bec23638709cad2
Frank Filius <f.filius@amsterdam.nl>
2026-05-26 14:03:22 +0200

    Fix conditional

--- src/modules/parking/components/TimeDifferenceNotice.tsx
+++ src/modules/parking/components/TimeDifferenceNotice.tsx
@@ -9,10 +9,10 @@ export const TimeDifferenceNotice = (props: Props) => {
     Boolean(props.serverTime),
   )
 
-  return isSameTime || props.isSameTime ? null : (
+  return !(isSameTime && props.isSameTime) ? (
     <Notice
       text={`We gebruiken de tijd in Nederland. Daar is het nu ${dayjs(serverTime ?? props.serverTime).format('HH:mm')} uur.`}
       variant="warning"
     />
-  )
+  ) : null
 }
