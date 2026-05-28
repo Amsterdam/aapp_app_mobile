b81fe58b1255b4910f0f7e717235c396e5e36ba2
Frank Filius <f.filius@amsterdam.nl>
2026-05-28 19:08:34 +0200

    Show date as well when differs from servertime date

--- src/modules/parking/components/TimeDifferenceNotice.tsx
+++ src/modules/parking/components/TimeDifferenceNotice.tsx
@@ -8,6 +8,7 @@ export const TimeDifferenceNotice = (props: Props) => {
   const {isSameTime, serverTime} = useIsLocalTimeSameAsServerTime(
     Boolean(props.serverTime),
   )
+  const isSameDay = dayjs(serverTime ?? props.serverTime).isSame(dayjs(), 'day')
 
   const showNotice =
     props.isSameTime !== undefined
@@ -16,7 +17,7 @@ export const TimeDifferenceNotice = (props: Props) => {
 
   return showNotice ? (
     <Notice
-      text={`We gebruiken de tijd in Nederland. Daar is het nu ${dayjs(serverTime ?? props.serverTime).format('HH:mm')} uur.`}
+      text={`We gebruiken de tijd in Nederland. Daar is het nu ${dayjs(serverTime ?? props.serverTime).format(isSameDay ? 'HH:mm' : 'DD MMM, HH:mm')} uur.`}
       variant="warning"
     />
   ) : null
