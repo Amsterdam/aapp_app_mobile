66f024157c8864a7b8ac153738c5dd44ab1992d0
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-05-28 12:10:11 +0200

    Add mapping of props

--- src/modules/contact/components/NewsletterSignup.tsx
+++ src/modules/contact/components/NewsletterSignup.tsx
@@ -1,9 +1,32 @@
+import type {ButtonProps} from '@/components/ui/buttons/Button'
 import {ExternalLinkButton} from '@/components/ui/buttons/ExternalLinkButton'
-import {Box} from '@/components/ui/containers/Box'
+import {Box, type BoxProps} from '@/components/ui/containers/Box'
 import {Column} from '@/components/ui/layout/Column'
 import {Paragraph} from '@/components/ui/text/Paragraph'
 import {Title} from '@/components/ui/text/Title'
 import {RedirectKey} from '@/modules/redirects/types'
+
+type Variants = 'news' | 'contact'
+
+const variantPropsMap: Record<
+  Variants,
+  {
+    boxVariant: BoxProps['variant']
+    buttonVariant: ButtonProps['variant']
+    text: string
+  }
+> = {
+  contact: {
+    text: 'Schrijf u nu in voor de Nieuwsbrief Amsterdam en ontvang wekelijks nieuws, tips en mooie verhalen over de stad en uw stadsdeel.',
+    boxVariant: 'primary',
+    buttonVariant: 'secondary',
+  },
+  news: {
+    text: 'Schrijf u nu in voor de Nieuwsbrief Amsterdam en ontvang wekelijks nieuws uit de stad en uw stadsdeel.',
+    boxVariant: 'positive',
+    buttonVariant: 'positive',
+  },
+}
 
 export const NewsletterSignup = ({
   variant = 'contact',
@@ -13,27 +36,20 @@ export const NewsletterSignup = ({
   <Box>
     <Box
       inset="lg"
-      variant={variant === 'news' ? 'positive' : 'primary'}>
+      variant={variantPropsMap[variant].boxVariant}>
       <Column gutter="md">
         <Title
           color="inverse"
           level="h2"
           text="Blijf op de hoogte!"
         />
-        <Paragraph color="inverse">
-          Schrijf u nu in voor de Nieuwsbrief Amsterdam en ontvang wekelijks
-          nieuws
-          {variant === 'contact'
-            ? ', tips en mooie verhalen over'
-            : ' uit'} de
-          stad en uw stadsdeel.
-        </Paragraph>
+        <Paragraph color="inverse">{variantPropsMap[variant].text}</Paragraph>
         <ExternalLinkButton
           accessibilityHint="Opent een link naar een aanmeldformulier voor de nieuwsbrief."
           label="Ik wil de nieuwsbrief"
           redirectKey={RedirectKey.contactNewsletterSignup}
           testID="ContactNewsletterSignupExternalLinkButton"
-          variant={variant === 'news' ? 'positive' : 'secondary'}
+          variant={variantPropsMap[variant].buttonVariant}
         />
       </Column>
     </Box>
