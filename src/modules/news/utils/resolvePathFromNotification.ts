67dc80458c23d23e705358c0ad9e4ccb5bd11a73
RikSchefferAmsterdam <r.scheffer@amsterdam.nl>
2026-06-10 21:24:43 +0200

    Add unit tests

--- src/modules/news/utils/resolvePathFromNotification.ts
+++ src/modules/news/utils/resolvePathFromNotification.ts
@@ -2,9 +2,11 @@ import {ModuleClientConfig} from '@/modules/types'
 
 export type PushNotificationType = 'news:new-liveblog' | 'news:liveblog-update'
 
-export const resolvePathFromNotification: ModuleClientConfig<{
-  type?: PushNotificationType
-}>['resolvePathFromNotification'] = notification => {
+export const resolvePathFromNotification: Required<
+  ModuleClientConfig<{
+    type?: PushNotificationType
+  }>
+>['resolvePathFromNotification'] = notification => {
   if (!notification?.data?.type || !notification?.data?.linkSourceid) {
     return
   }
