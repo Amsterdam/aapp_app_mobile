63f1db02d82bf9a8c3ca54ee2bff70ef927f8948
Frank Filius <f.filius@amsterdam.nl>
2026-06-17 16:31:45 +0200

    Stop navigating without button being pressed first

--- src/modules/parking/components/select-permit/AdditionalLoginButton.tsx
+++ src/modules/parking/components/select-permit/AdditionalLoginButton.tsx
@@ -1,20 +1,31 @@
-import {useCallback} from 'react'
+import {useCallback, useEffect, useState} from 'react'
 import {Button, type ButtonProps} from '@/components/ui/buttons/Button'
 import {useNavigation} from '@/hooks/navigation/useNavigation'
 import {useDispatch} from '@/hooks/redux/useDispatch'
 import {ParkingRouteName} from '@/modules/parking/routes'
-import {setIsLoggingIn} from '@/modules/parking/slice'
+import {
+  setIsLoggingIn,
+  useParkingAccountIsLoggingIn,
+} from '@/modules/parking/slice'
 
 type Props = ButtonProps
 
 export const AdditionalLoginButton = (props: Props) => {
   const dispatch = useDispatch()
   const {navigate} = useNavigation()
+  const isLoggingIn = useParkingAccountIsLoggingIn()
+  const [isPressed, setIsPressed] = useState(false)
 
   const onPress = useCallback(() => {
     dispatch(setIsLoggingIn(true))
-    navigate(ParkingRouteName.login)
-  }, [dispatch, navigate])
+    setIsPressed(true)
+  }, [dispatch])
+
+  useEffect(() => {
+    if (isLoggingIn && isPressed) {
+      navigate(ParkingRouteName.login)
+    }
+  }, [isLoggingIn, isPressed, navigate])
 
   return (
     <Button
