526c2baa6158aca84fdda7d8009a795728e7f4f1
Frank Filius <f.filius@amsterdam.nl>
2026-06-17 15:37:44 +0200

    Navigate to login screen when user presses

--- src/modules/parking/components/select-permit/AdditionalLoginButton.tsx
+++ src/modules/parking/components/select-permit/AdditionalLoginButton.tsx
@@ -1,29 +1,20 @@
-import {useCallback, useEffect} from 'react'
+import {useCallback} from 'react'
 import {Button, type ButtonProps} from '@/components/ui/buttons/Button'
 import {useNavigation} from '@/hooks/navigation/useNavigation'
 import {useDispatch} from '@/hooks/redux/useDispatch'
 import {ParkingRouteName} from '@/modules/parking/routes'
-import {
-  setIsLoggingIn,
-  useParkingAccountIsLoggingIn,
-} from '@/modules/parking/slice'
+import {setIsLoggingIn} from '@/modules/parking/slice'
 
 type Props = ButtonProps
 
 export const AdditionalLoginButton = (props: Props) => {
   const dispatch = useDispatch()
   const {navigate} = useNavigation()
-  const isLoggingIn = useParkingAccountIsLoggingIn()
 
   const onPress = useCallback(() => {
     dispatch(setIsLoggingIn(true))
-  }, [dispatch])
-
-  useEffect(() => {
-    if (isLoggingIn) {
-      navigate(ParkingRouteName.login)
-    }
-  }, [isLoggingIn, navigate])
+    navigate(ParkingRouteName.login)
+  }, [dispatch, navigate])
 
   return (
     <Button
