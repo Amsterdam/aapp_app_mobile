d154b7b53daa95ec47013071df92e30ce49b1f9e
RikSchefferAmsterdam <r.scheffer@amsterdam.nl>
2026-06-25 22:02:16 +0200

    formatting

--- src/modules/boat-charging/useIsLoggedIn.ts
+++ src/modules/boat-charging/useIsLoggedIn.ts
@@ -1,7 +1,8 @@
 import {useOpenIdConnectAuth} from '@/modules/boat-charging/hooks/useOpenIdConnectAuth'
 
 export const useIsLoggedIn = () => {
-  const {hasValidAccessToken, isAuthenticated, username} = useOpenIdConnectAuth()
+  const {hasValidAccessToken, isAuthenticated, username} =
+    useOpenIdConnectAuth()
 
   return {isLoggedIn: isAuthenticated && hasValidAccessToken, username}
 }
