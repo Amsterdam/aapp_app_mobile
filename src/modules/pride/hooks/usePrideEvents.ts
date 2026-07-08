e9459fb1d82c32eda9fe251836318a82b6d7003e
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-07-08 14:43:18 +0200

    Add extraPoints on map

--- src/modules/pride/hooks/usePrideEvents.ts
+++ src/modules/pride/hooks/usePrideEvents.ts
@@ -1,5 +1,6 @@
-import {useCallback} from 'react'
+import {useCallback, useMemo} from 'react'
 import type {PrideEvent} from '@/modules/pride/types'
+import type {Coordinates} from '@/types/location'
 import {usePrideEventsQuery} from '@/modules/pride/service'
 
 export const usePrideEvents = () => {
@@ -10,9 +11,55 @@ export const usePrideEvents = () => {
     [events],
   )
 
+  const eventsByLocation = useMemo(() => {
+    if (!events) return
+
+    return events.reduce(
+      (
+        locations: Record<
+          string,
+          {
+            coordinates: Coordinates
+            events: PrideEvent[]
+          }
+        >,
+        event,
+      ) => {
+        if (!event.address.coordinates?.lat) {
+          return locations
+        }
+
+        const coordsKey =
+          event.address.coordinates.lat.toString() +
+          event.address.coordinates.lon.toString()
+
+        const locationIsAlreadyIncluded = locations[coordsKey]
+
+        if (locationIsAlreadyIncluded) {
+          return {
+            ...locations,
+            [coordsKey]: {
+              coordinates: event.address.coordinates,
+              events: [...locations[coordsKey].events, event],
+            },
+          }
+        }
+
+        locations[coordsKey] = {
+          coordinates: event.address.coordinates,
+          events: [event],
+        }
+
+        return locations
+      },
+      {},
+    )
+  }, [events])
+
   return {
     events,
     getEvent,
+    eventsByLocation,
     ...rest,
   }
 }
--- src/modules/pride/screens/PrideDetails.screen.tsx
+++ src/modules/pride/screens/PrideDetails.screen.tsx
@@ -1,9 +1,10 @@
 import type {NavigationProps} from '@/app/navigation/types'
-import type {PrideRouteName} from '@/modules/pride/routes'
 import {BottomSheet} from '@/components/features/bottom-sheet/BottomSheet'
 import {MapViewSwitchProvider} from '@/components/features/map/providers/MapViewSwitchProvider'
 import {MapFocus} from '@/components/features/map/types'
 import {Screen} from '@/components/features/screen/Screen'
+import {usePrideEvents} from '@/modules/pride/hooks/usePrideEvents'
+import {PrideRouteName} from '@/modules/pride/routes'
 import {ServiceHeaderButton} from '@/modules/service/components/ServiceHeaderButton'
 import {ServicePointView} from '@/modules/service/components/ServicePointView'
 import {createBottomsheetVariants} from '@/modules/service/components/bottomsheet/bottomsheetVariants'
@@ -11,31 +12,35 @@ import {ServiceMapFiltersProvider} from '@/modules/service/providers/ServiceMapF
 
 type Props = NavigationProps<PrideRouteName.details>
 
-export const PrideDetailsScreen = ({route}: Props) => (
-  <MapViewSwitchProvider>
-    <ServiceMapFiltersProvider serviceId={route.params.id}>
-      <Screen
-        bottomSheet={
-          <BottomSheet
-            scroll
-            testID="PrideDetailsScreenBottomSheet"
-            variants={createBottomsheetVariants(route.params.id)}
-            withCloseButton
+export const PrideDetailsScreen = ({route}: Props) => {
+  usePrideEvents()
+
+  return (
+    <MapViewSwitchProvider>
+      <ServiceMapFiltersProvider serviceId={route.params.id}>
+        <Screen
+          bottomSheet={
+            <BottomSheet
+              scroll
+              testID="PrideDetailsScreenBottomSheet"
+              variants={createBottomsheetVariants(route.params.id)}
+              withCloseButton
+            />
+          }
+          headerOptions={{
+            disableHorizontalInsets: true,
+            headerTitle: route.params.title,
+            SideComponent: ServiceHeaderButton,
+          }}
+          scroll={false}
+          testID="PrideDetailsScreen"
+          withBottomInset={false}>
+          <ServicePointView
+            focusType={MapFocus.amsterdam}
+            id={route.params.id}
           />
-        }
-        headerOptions={{
-          disableHorizontalInsets: true,
-          headerTitle: route.params.title,
-          SideComponent: ServiceHeaderButton,
-        }}
-        scroll={false}
-        testID="PrideDetailsScreen"
-        withBottomInset={false}>
-        <ServicePointView
-          focusType={MapFocus.amsterdam}
-          id={route.params.id}
-        />
-      </Screen>
-    </ServiceMapFiltersProvider>
-  </MapViewSwitchProvider>
-)
+        </Screen>
+      </ServiceMapFiltersProvider>
+    </MapViewSwitchProvider>
+  )
+}
