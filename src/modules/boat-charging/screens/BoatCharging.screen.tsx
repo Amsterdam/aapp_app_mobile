5fd0455769c47d902e5a356535fe1a1152584723
Frank Filius <f.filius@amsterdam.nl>
2026-07-22 13:53:59 +0200

    Add bar to map and list screen

--- src/modules/boat-charging/screens/BoatCharging.screen.tsx
+++ src/modules/boat-charging/screens/BoatCharging.screen.tsx
@@ -4,26 +4,31 @@ import {Screen} from '@/components/features/screen/Screen'
 import {BoatChargingHeaderButton} from '@/modules/boat-charging/components/BoatChargingHeaderButton'
 import {BoatChargingView} from '@/modules/boat-charging/components/BoatChargingView'
 import {bottomsheetVariants} from '@/modules/boat-charging/components/bottomsheet/bottomsheetVariants'
+import {BoatChargingSessionBar} from '@/modules/boat-charging/components/session/BoatChargingSessionBar'
+import {BoatChargingSessionsProvider} from '@/modules/boat-charging/providers/BoatChargingSessions.provider'
 
 export const BoatChargingScreen = () => (
-  <MapViewSwitchProvider>
-    <Screen
-      bottomSheet={
-        <BottomSheet
-          scroll
-          testID="BoatChargingPointBottomSheet"
-          variants={bottomsheetVariants}
-          withCloseButton
-        />
-      }
-      headerOptions={{
-        headerTitle: 'Boot laden',
-        SideComponent: BoatChargingHeaderButton,
-      }}
-      scroll={false}
-      testID="BoatChargingScreen"
-      withBottomInset={false}>
-      <BoatChargingView />
-    </Screen>
-  </MapViewSwitchProvider>
+  <BoatChargingSessionsProvider>
+    <MapViewSwitchProvider>
+      <Screen
+        bottomSheet={
+          <BottomSheet
+            scroll
+            testID="BoatChargingPointBottomSheet"
+            variants={bottomsheetVariants}
+            withCloseButton
+          />
+        }
+        headerOptions={{
+          headerTitle: 'Boot laden',
+          SideComponent: BoatChargingHeaderButton,
+        }}
+        scroll={false}
+        stickyHeader={<BoatChargingSessionBar />}
+        testID="BoatChargingScreen"
+        withBottomInset={false}>
+        <BoatChargingView />
+      </Screen>
+    </MapViewSwitchProvider>
+  </BoatChargingSessionsProvider>
 )
