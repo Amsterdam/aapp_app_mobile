3080480e1556d3dad37fced603d80546417bfc6f
RikSchefferAmsterdam <r.scheffer@amsterdam.nl>
2026-08-03 15:22:12 +0200

    improve

--- src/modules/boat-charging/providers/BoatChargingSession.provider.tsx
+++ src/modules/boat-charging/providers/BoatChargingSession.provider.tsx
@@ -85,18 +85,14 @@ export const BoatChargingSessionProvider = ({
   const {data: settingsServerData} = useBoatChargingSettingsQuery()
   const settings: BoatChargingSettings = useMemo(
     () =>
-      settingsServerData
-        ? {
-            ...settingsServerData,
-          }
-        : {
-            pre_authorization_amount: null,
-            session_cleanup_enabled: null,
-            session_expiry_hours: 24,
-            session_expiry_warning_hours: 20,
-            standard_fine: null,
-            vat_fraction: 1.21,
-          },
+      settingsServerData || {
+        pre_authorization_amount: null,
+        session_cleanup_enabled: null,
+        session_expiry_hours: 24,
+        session_expiry_warning_hours: 20,
+        standard_fine: null,
+        vat_fraction: 1.21,
+      },
     [settingsServerData],
   )
 
