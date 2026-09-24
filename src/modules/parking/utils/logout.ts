ddfe2bc4f956adacb12f8cd78f9b42fadf441862
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-09-24 10:12:38 +0200

    Remove Error throwing when no reportCode or scope found

--- src/modules/parking/utils/logout.ts
+++ src/modules/parking/utils/logout.ts
@@ -24,7 +24,7 @@ export const logout = async (
   const {reportCode, scope} = parkingAccount || {}
 
   if (!reportCode || !scope) {
-    throw new Error('No report code or scope found for this parking account')
+    return
   }
 
   dispatch(parkingSlice.actions.setIsLoggingOut(true))
