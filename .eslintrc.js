011a5aae311a6a6dc9a6fbb5ee5e717e5a4afd3b
Wouter van der Laan <w.van.der.laan@amsterdam.nl>
2026-09-25 10:06:06 +0200

    Add TODO for temp disabling of eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,7 +49,7 @@ module.exports = {
         'sonarjs/parameterized-tests': 'warn',
         'sonarjs/assertions-in-tests': 'warn',
         'sonarjs/no-redundant-optional': 'warn',
-        'sonarjs/synchronous-suite-callback': 'off', // eslint-plugin-sonarjs@4.2.1 uses sourceCode.getRange, which is unavailable in ESLint 8
+        'sonarjs/synchronous-suite-callback': 'off', // TODO: eslint-plugin-sonarjs@4.2.1 uses sourceCode.getRange, which is unavailable in ESLint 8. Remove this line when new version of plugin is available and error is resolved.
         'sonarjs/aws-restricted-ip-admin-access': 'off',
         'sonarjs/deprecation': 'off',
         'sonarjs/function-return-type': 'off', // temporarily off because of the high number of hits
